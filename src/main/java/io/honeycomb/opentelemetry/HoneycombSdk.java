3a38d35b7508ee58038a4f43584cf3ad98809fa9
Paul Osman <paul@honeycomb.io>
2021-04-05 17:17:38 -0500

    Add comments

--- src/main/java/io/honeycomb/opentelemetry/HoneycombSdk.java
+++ src/main/java/io/honeycomb/opentelemetry/HoneycombSdk.java
@@ -5,6 +5,12 @@ import io.opentelemetry.api.trace.TracerProvider;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 
+/**
+ * The Honeycomb SDK implementation of {@link OpenTelemetry}.
+ *
+ * This class exists to make it easier and more intuitive to use
+ * Honeycomb with OpenTelemetry.
+ */
 public final class HoneycombSdk implements OpenTelemetry {
 
     private final SdkTracerProvider tracerProvider;
@@ -15,6 +21,10 @@ public final class HoneycombSdk implements OpenTelemetry {
         this.propagators = propagators;
     }
 
+    /**
+     * Returns a new {@link HoneycombSdkBuilder} for configuring an instance of {@linkplain
+     * HoneycombSdk the OpenTelemetry SDK}.
+     */
     public static HoneycombSdkBuilder builder() {
         return new HoneycombSdkBuilder();
     }
--- src/main/java/io/honeycomb/opentelemetry/HoneycombSdkBuilder.java
+++ src/main/java/io/honeycomb/opentelemetry/HoneycombSdkBuilder.java
@@ -9,6 +9,11 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 
+import java.util.Objects;
+
+/**
+ * A builder for configuring an {@link HoneycombSdk}
+ */
 public final class HoneycombSdkBuilder {
 
     private final String HoneycombTeamHeader = "X-Honeycomb-Team";
@@ -20,34 +25,95 @@ public final class HoneycombSdkBuilder {
     private String dataset;
     private String endpoint;
 
+    /**
+     * Prevent direct initialization. Should be instantiated by calling the
+     * builder method in {@link HoneycombSdk}.
+     * */
     HoneycombSdkBuilder() {}
 
+    /**
+     * Sets the Honeycomb API Key to use.
+     *
+     * <p>This value is sent to Honeycomb on every request and is used to identify
+     * the team making a request.</p>
+     *
+     * @param apiKey a String to use as the API key. See team settings in Honeycomb.
+     * @return HoneycombSdkBuilder instance.
+     */
     public HoneycombSdkBuilder setApiKey(String apiKey) {
         this.apiKey = apiKey;
         return this;
     }
 
+    /**
+     * Sets the Honeycomb dataset to use.
+     *
+     * <p>This value is sent to Honeycomb on every request and is used to identify
+     * the dataset that trace data is being written to.</p>
+     *
+     * @param dataset a String to use as the dataset name.
+     *
+     * @return HoneycombSdkBuilder instance.
+     */
     public HoneycombSdkBuilder setDataset(String dataset) {
         this.dataset = dataset;
         return this;
     }
 
+    /**
+     * Sets the Honeycomb endpoint to use. Optional, defaults to the Honeycomb ingest API.
+     *
+     * @param endpoint a String to use as the endpoint URI. Must begin with https or http.
+     *
+     * @return HoneycombSdkBuilder instance.
+     */
     public HoneycombSdkBuilder setEndpoint(String endpoint) {
         this.endpoint = endpoint;
         return this;
     }
 
+    /**
+     * Sets the {@link ContextPropagators} to use.
+     *
+     * <p>Note that if none are specified, {@link W3CTraceContextPropagator} will be used
+     * by default.</p>
+     *
+     * @return HoneycombSdkBuilder instance.
+     */
     public HoneycombSdkBuilder setContextPropagators(ContextPropagators propagators) {
         this.propagators = propagators;
         return this;
     }
 
+    /**
+     * Returns a new {@link HoneycombSdk} built with the configuration of this {@link
+     * HoneycombSdkBuilder} and registers it as the global {@link
+     * io.opentelemetry.api.OpenTelemetry}. An exception will be thrown if this method is attempted to
+     * be called multiple times in the lifecycle of an application - ensure you have only one SDK for
+     * use as the global instance. If you need to configure multiple SDKs for tests, use {@link
+     * GlobalOpenTelemetry#resetForTest()} between them.
+     *
+     * @see GlobalOpenTelemetry
+     */
     public HoneycombSdk buildAndRegisterGlobal() {
         HoneycombSdk sdk = build();
         GlobalOpenTelemetry.set(sdk);
         return sdk;
     }
 
+    /**
+     * Returns a new {@link HoneycombSdk} built with the configuration of this {@link
+     * HoneycombSdkBuilder}. This SDK is not registered as the global {@link
+     * io.opentelemetry.api.OpenTelemetry}. It is recommended that you register one SDK using {@link
+     * HoneycombSdkBuilder#buildAndRegisterGlobal()} for use by instrumentation that requires
+     * access to a global instance of {@link io.opentelemetry.api.OpenTelemetry}.
+     *
+     * <p>Note that the {@link HoneycombSdkBuilder} automatically assigns a
+     * {@link SdkTracerProvider} with a {@link BatchSpanProcessor} that has an
+     * {@link OtlpGrpcSpanExporter} configured.</p>
+     *
+     * @see GlobalOpenTelemetry
+     */
     public HoneycombSdk build() {
         OtlpGrpcSpanExporterBuilder builder = OtlpGrpcSpanExporter.builder();
 
