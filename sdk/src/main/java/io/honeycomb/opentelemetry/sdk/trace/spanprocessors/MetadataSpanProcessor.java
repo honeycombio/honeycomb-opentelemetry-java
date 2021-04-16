c583716479b0552d293c0c9954145f39794abc34
Paul Osman <paul@honeycomb.io>
2021-04-16 08:54:12 -0500

    Cache version in span processor

--- sdk/src/main/java/io/honeycomb/opentelemetry/sdk/trace/spanprocessors/MetadataSpanProcessor.java
+++ sdk/src/main/java/io/honeycomb/opentelemetry/sdk/trace/spanprocessors/MetadataSpanProcessor.java
@@ -6,21 +6,36 @@ import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class MetadataSpanProcessor implements SpanProcessor {
 
     private final String HONEYCOMB_SDK_VERSION_ATTRIBUTE = "honeycomb.meta.sdk.version";
     private final String HONEYCOMB_SDK_VERSION_PREFIX = "Honeycomb-OpenTelemetry-Java";
+    private final Map<String, String> metadataCache = new HashMap<String, String>();
 
-    @Override
-    public void onStart(Context parentContext, ReadWriteSpan span) {
+    private String getVersion() {
+        if (metadataCache.containsKey("version")) {
+            return metadataCache.get("version");
+        }
+
         final Properties properties = new Properties();
         try {
             properties.load(this.getClass().getClassLoader().getResourceAsStream("sdk.properties"));
-        } catch (IOException ignored) { }
-        String version = properties.getProperty("version");
-        span.setAttribute(HONEYCOMB_SDK_VERSION_ATTRIBUTE, HONEYCOMB_SDK_VERSION_PREFIX + " " + version);
+        } catch (IOException e) {
+            return "unknown-version";
+        }
+
+        return properties.getProperty("version");
+    }
+
+    @Override
+    public void onStart(Context parentContext, ReadWriteSpan span) {
+        span.setAttribute(
+            HONEYCOMB_SDK_VERSION_ATTRIBUTE,
+            HONEYCOMB_SDK_VERSION_PREFIX + " " + getVersion());
     }
 
     @Override
