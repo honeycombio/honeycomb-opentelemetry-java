5b5b7999c9049e080f7ded724de347694652eb1f
JamieDanielson <jamieedanielson@gmail.com>
2021-09-08 14:05:26 -0400

    Adds custom span to agent example

--- examples/spring-agent/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
+++ examples/spring-agent/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
@@ -11,12 +11,20 @@ import io.opentelemetry.extension.annotations.WithSpan;
 
 @RestController
 public class HelloController {
+    private static final String importantInfo = "Important Information";
 
     @GetMapping("/")
-    @WithSpan // just adds an extra span for now
       public String index() {
         Span span = Span.current();
         span.setAttribute("custom_field", "important value");
-		return "Greetings from Spring Boot!";
+        String intro = getImportantInfo();
+        String finalMessage = String.format("%s: Greetings from Spring Boot!", intro);
+        return finalMessage;
 	}
+
+    @WithSpan("importantSpan")
+    public String getImportantInfo() {
+        return importantInfo;
+    }
+
 }
