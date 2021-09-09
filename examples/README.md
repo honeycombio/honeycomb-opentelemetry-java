d8ba7332cd2b9dd3709b6ed2da745d6596398206
JamieDanielson <jamieedanielson@gmail.com>
2021-09-09 18:22:48 -0400

    Update examples to use local code

--- examples/README.md
+++ examples/README.md
@@ -4,9 +4,10 @@
 
 To use the examples:
 
+- Clone or download the entire repo, as they rely on local code instead of Maven Central
 - Add environment variables (or system properties)
-- From the spring-agent or spring-sdk directory run `./gradlew bootRun`
-- `curl localhost:8080` to get 'Greetings from Spring Boot!'
+- From the spring-agent or spring-sdk directory run `../../gradlew bootRun`
+- `curl localhost:8080` to get 'Important Information: Greetings from Spring Boot!'
 
 ### spring-agent
 
--- examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/HelloController.java
+++ examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/HelloController.java
@@ -8,25 +8,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.extension.annotations.WithSpan;
 
 @RestController
 public class HelloController {
     private static final String importantInfo = "Important Information";
 
-  @Autowired
-  private HoneycombSdk sdk;
+    @Autowired
+    private HoneycombSdk sdk;
 
-	@GetMapping("/")
-	public String index() {
-    Tracer tracer = sdk.getTracer("examples");
-    Span span = tracer.spanBuilder("greetings").startSpan();
-    span.setAttribute("custom_field", "important value");
-    String intro = getImportantInfo();
-    String finalMessage = String.format("%s: Greetings from Spring Boot!", intro);
-    span.end();
-    return finalMessage;
-	}
+    @GetMapping("/")
+    public String index() {
+        Tracer tracer = sdk.getTracer("examples");
+        Span span = tracer.spanBuilder("greetings").startSpan();
+        span.setAttribute("custom_field", "important value");
+        String intro = getImportantInfo();
+        String finalMessage = String.format("%s: Greetings from Spring Boot!", intro);
+        span.end();
+        return finalMessage;
+    }
 
     public String getImportantInfo() {
         return importantInfo;
