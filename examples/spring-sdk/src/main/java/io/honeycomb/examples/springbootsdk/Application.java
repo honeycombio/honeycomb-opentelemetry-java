a26a0e152ad8514b08679b317b7c0f1f02cd317b
Mike Goldsmth <goldsmith.mike@gmail.com>
2021-09-15 10:30:53 +0100

    update examples to work with new interface

--- examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
+++ examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
@@ -2,8 +2,9 @@ package io.honeycomb.examples.springbootsdk;
 
 import java.util.Arrays;
 
-import io.honeycomb.opentelemetry.HoneycombSdk;
+import io.honeycomb.opentelemetry.OpenTelemetryConfiguration;
 import io.honeycomb.opentelemetry.sdk.trace.spanprocessors.BaggageSpanProcessor;
+import io.opentelemetry.api.OpenTelemetry;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,14 +16,14 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
 	@Bean
-	public HoneycombSdk honeycomb() {
-			return new HoneycombSdk.Builder()
-					.addSpanProcessor(new BaggageSpanProcessor())
-					.setApiKey(System.getenv("HONEYCOMB_API_KEY"))
-                    .setDataset(System.getenv("HONEYCOMB_DATASET"))
-					.setServiceName("example-service")
-					.setEndpoint(System.getenv("HONEYCOMB_API_ENDPOINT"))
-					.buildAndRegisterGlobal();
+	public OpenTelemetry honeycomb() {
+			return OpenTelemetryConfiguration.builder()
+				.addSpanProcessor(new BaggageSpanProcessor())
+				.setApiKey(System.getenv("HONEYCOMB_API_KEY"))
+				.setDataset(System.getenv("HONEYCOMB_DATASET"))
+				.setServiceName("example-service")
+				.setEndpoint(System.getenv("HONEYCOMB_API_ENDPOINT"))
+				.buildAndRegisterGlobal();
 	}
 
 	public static void main(String[] args) {
