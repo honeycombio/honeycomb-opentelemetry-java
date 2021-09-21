f191b81a7c00a1922dfaf2032381fb2c3ae45cef
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-09-21 13:58:06 +0100

    Set spring boot port and update examples formatting (#137)
    Spring boot applications always try to start on port 8080. This can sometimes conflict with other running processes and it's nice to have a way to change the default port. It also fixes up some formatting (eg tabs -> spaces).
    
    Co-authored-by: JamieDanielson <jamieedanielson@gmail.com>

--- examples/spring-agent/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
+++ examples/spring-agent/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
@@ -11,13 +11,13 @@ public class HelloController {
     private static final String importantInfo = "Important Information";
 
     @GetMapping("/")
-      public String index() {
+    public String index() {
         Span span = Span.current();
         span.setAttribute("custom_field", "important value");
         String intro = getImportantInfo();
         String finalMessage = String.format("%s: Greetings from Spring Boot!", intro);
         return finalMessage;
-	}
+    }
 
     @WithSpan("importantSpan")
     public String getImportantInfo() {
