69f3bb6437c93f14b90794c1d7a3e4f39d355d1e
JamieDanielson <jamieedanielson@gmail.com>
2021-10-20 11:57:57 -0400

    baby steps

--- smoke-tests/src/test/java/io/honeycomb/javaagent/smoketest/SmokeTest.java
+++ smoke-tests/src/test/java/io/honeycomb/javaagent/smoketest/SmokeTest.java
@@ -8,10 +8,17 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.MountableFile;
 
 abstract class SmokeTest {
     private static final Logger logger = LoggerFactory.getLogger(SmokeTest.class);
     private static final Network network = Network.newNetwork();
+
+    // CHECK THIS OUT FOR INSPO YAY
+    // https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/examples/distro/smoke-tests/src/test/java/com/example/javaagent/smoketest
+
+    // TODO - IS THIS WHERE THE JAR IS?
+    protected static final String agentPath = System.getProperty("io.opentelemetry.smoketest.agent.shadowJar.path");
 
     // The a fake backend to receive OTLP data
     protected static GenericContainer<?> backend;
@@ -27,6 +34,16 @@ abstract class SmokeTest {
                 .withNetworkAliases("backend")
                 .withLogConsumer(new Slf4jLogConsumer(logger));
         backend.start();
+    }
+
+    protected static GenericContainer<?> agentOnlyApp;
+
+    // TODO - WHATS THE REST?
+    static void startAgentOnlyApp() {
+        agentOnlyApp = new GenericContainer<>(
+            "openjdk:17-jdk-alpine")
+            .withCopyFileToContainer(
+                MountableFile.forHostPath(agentPath), "/spring-agent-only*.jar")
     }
 
     @AfterAll
