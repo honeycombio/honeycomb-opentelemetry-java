4b6031eb9058c6ddeda149d1f59f047154786607
JamieDanielson <jamieedanielson@gmail.com>
2021-11-03 15:47:13 -0400

    Simplify example output (#193)

--- examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
+++ examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
@@ -27,18 +27,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-        };
+        System.out.println("OK I'm ready now!");
     }
 }
