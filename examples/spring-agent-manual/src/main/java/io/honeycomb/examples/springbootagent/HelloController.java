fabd6b8c89eddd5a67d30b6c61b4ffdd51e44f79
JamieDanielson <jamieedanielson@gmail.com>
2021-11-03 16:49:26 -0400

    Rename example spring-agent to spring-agent-manual

--- examples/spring-agent-manual/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
+++ examples/spring-agent-manual/src/main/java/io/honeycomb/examples/springbootagent/HelloController.java
@@ -1,4 +1,4 @@
-package io.honeycomb.examples.springbootagent;
+package io.honeycomb.examples.springbootagentmanual;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
