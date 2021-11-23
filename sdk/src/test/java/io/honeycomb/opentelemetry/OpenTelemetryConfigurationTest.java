238da6f26835ccb2c486bb49faf9421f9a0cecfa
Mike Goldsmith <goldsmith.mike@gmail.com>
2021-11-23 17:13:54 +0000

    Update SDK tests to validate returned OpenTelemtry instance (#210)
    Co-authored-by: JamieDanielson <jamieedanielson@gmail.com>
    Co-authored-by: Vera Reynolds <vreynolds@users.noreply.github.com>
    Co-authored-by: Robb Kidd <robbkidd@honeycomb.io>

--- sdk/src/test/java/io/honeycomb/opentelemetry/OpenTelemetryConfigurationTest.java
+++ sdk/src/test/java/io/honeycomb/opentelemetry/OpenTelemetryConfigurationTest.java
@@ -4,7 +4,19 @@ import io.honeycomb.opentelemetry.sdk.trace.samplers.DeterministicTraceSampler;
 import io.honeycomb.opentelemetry.sdk.trace.spanprocessors.BaggageSpanProcessor;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.TracerProvider;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.SpanProcessor;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -18,56 +30,60 @@ public class OpenTelemetryConfigurationTest {
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     public void testConfiguration_tracerSettings() {
         Sampler sampler = new DeterministicTraceSampler(5);
-        BaggageSpanProcessor baggageSpanProcessor = new BaggageSpanProcessor();
         OpenTelemetry openTelemetry = OpenTelemetryConfiguration.builder()
             .setSampler(sampler)
-            .addSpanProcessor(baggageSpanProcessor)
             .setApiKey("foobar")
             .setDataset("dataset")
-            .build();
-
-        // TODO:
-        // Figure out a way to ensure that spans emitted with this
-        // configuration have a sample.rate attribute with a value
-        // of 5.
-        Assertions.assertNotNull(sampler);
-        Assertions.assertNotNull(baggageSpanProcessor);
-        Assertions.assertNotNull(openTelemetry);
-    }
-
-    @Test
-    void testConfiguration_headers() {
-        OpenTelemetry openTelemetry = OpenTelemetryConfiguration.builder()
-            .setApiKey("foobar")
-            .setDataset("dataset")
-            .build();
-
-        Assertions.assertNotNull(openTelemetry);
-
-        // TODO:
-        // Figure out a way to test that the api key and dataset
-        // have been set as headers in the Otlp Exporter and are
-        // sent as gRPC metadata.
-    }
-
-    @Test
-    void testConfiguration_addAttributes() {
-        // NOTE: this does not verify the attributes are added correctly to
-        // the resource, but does exercise the interface to ensure it is
-        // can be used fluently (eg chain OpenTelemetryconfiguration.Builder calls)
-        OpenTelemetry openTelemetry = OpenTelemetryConfiguration.builder()
             .addResourceAttribute("str", "str")
             .addResourceAttribute("bool", true)
             .addResourceAttribute("int", 123)
             .addResourceAttribute("str-array", "str1", "str2", "str3")
             .build();
 
-        Assertions.assertNotNull(openTelemetry);
-        // TODO: Figure out way to retrieve configured Resource from TracerProvider
-        // to verify resource attributes have been added correctly
-        // eg it might be possible using reflection
-        // https://stackoverflow.com/questions/8267964/how-to-access-package-private-class-from-a-class-in-some-other-package
+        TracerProvider provider = openTelemetry.getTracerProvider();
+        try {
+            Method m = provider.getClass().getDeclaredMethod("unobfuscate");
+            m.setAccessible(true);
+            SdkTracerProvider sdkProvider = (SdkTracerProvider) m.invoke(provider, (Object[]) null);
+
+            // verify sampler
+            Sampler s = sdkProvider.getSampler();
+            Assertions.assertEquals(sampler, s);
+
+            Field providerField = pluckField(sdkProvider.getClass(), "sharedState");
+            Object sharedState = providerField.get(sdkProvider);
+            Field resourceField = pluckField(sharedState.getClass(), "resource");
+            Resource r = (Resource) resourceField.get(sharedState);
+            Attributes attrs = r.getAttributes();
+
+            // verify resource attributes
+            Assertions.assertEquals(DistroMetadata.VERSION_VALUE, attrs.get(AttributeKey.stringKey(DistroMetadata.VERSION_FIELD)));
+            Assertions.assertEquals(DistroMetadata.RUNTIME_VERSION_VALUE, attrs.get(AttributeKey.stringKey(DistroMetadata.RUNTIME_VERSION_FIELD)));
+            Assertions.assertEquals("str", attrs.get(AttributeKey.stringKey("str")));
+            Assertions.assertEquals(true, attrs.get(AttributeKey.booleanKey("bool")));
+            Assertions.assertEquals(123, attrs.get(AttributeKey.longKey("int")));
+            Assertions.assertEquals(Arrays.asList("str1", "str2", "str3"), attrs.get(AttributeKey.stringArrayKey("str-array")));
+
+            Field procesorField = pluckField(sharedState.getClass(), "activeSpanProcessor");
+
+            Object sp = procesorField.get(sharedState);
+            Field allSpanProcessors = pluckField(sp.getClass(), "spanProcessorsAll");
+            List<SpanProcessor> processors = (List<SpanProcessor>) allSpanProcessors.get(sp);
+
+            // verify configured span processors
+            Assertions.assertInstanceOf(BaggageSpanProcessor.class, processors.get(0));
+            Assertions.assertInstanceOf(BatchSpanProcessor.class, processors.get(1));
+        } catch (Exception e) {
+            Assertions.fail(e);
+        }
+    }
+
+    private Field pluckField(Class aClass, String fieldName) throws NoSuchFieldException, SecurityException {
+        Field pluckedField = aClass.getDeclaredField(fieldName);
+        pluckedField.setAccessible(true);
+        return pluckedField;
     }
 }
