d4fe37d26ac43187e99aabb901ea1abecbd5e579
Robb Kidd <robbkidd@honeycomb.io>
2022-04-06 14:12:03 -0400

    resolve deprecations from upstream (#278)
    * get a LoggingSpanExporter via create()
    
        The constructor LoggingSpanExporter() is deprecated.
        io.opentelemetry.exporter.logging.LoggingSpanExporter.LoggingSpanExporter()
        Deprecated Use create().
    
    OK.
    
    * change our autoconfig via SPI over to the new way
    
        io.opentelemetry.sdk.autoconfigure.spi.traces.SdkTracerProviderConfigurer
        Deprecated Use AutoConfigurationCustomizer.addTracerProviderCustomizer(BiFunction).
    
    This dep notice was not super clear on remediation advice. These changes
    are based on the update[1] to the otel-java-instrumentation's own demo
    distribution to address the deprecation.
    The autoconfiguration customizer was introduced to otel-java 1.11.0[2]
    via #4004[3].
    
    [1] https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/5574/files
    [2] https://github.com/open-telemetry/opentelemetry-java/releases/tag/v1.11.0
    [3] https://github.com/open-telemetry/opentelemetry-java/pull/4004

--- custom/src/main/java/io/honeycomb/opentelemetry/HoneycombAutoConfigurationCustomizerProvider.java
+++ custom/src/main/java/io/honeycomb/opentelemetry/HoneycombAutoConfigurationCustomizerProvider.java
@@ -3,17 +3,23 @@ package io.honeycomb.opentelemetry;
 import io.honeycomb.opentelemetry.sdk.trace.samplers.DeterministicTraceSampler;
 import io.honeycomb.opentelemetry.sdk.trace.spanprocessors.BaggageSpanProcessor;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
-import io.opentelemetry.sdk.autoconfigure.spi.traces.SdkTracerProviderConfigurer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
 import io.opentelemetry.sdk.trace.SdkTracerProviderBuilder;
 
 /**
- * Honeycomb implementation of {@link SdkTracerProviderConfigurer} SPI.
+ * Honeycomb implementation of {@link AutoConfigurationCustomizerProvider} SPI.
  *
  * This configurer adds a trace sampler and a span processor to the OpenTelemetry auto-instrumentation.
  */
-public class HoneycombSdkTracerProviderConfigurer implements SdkTracerProviderConfigurer {
+public class HoneycombAutoConfigurationCustomizerProvider implements AutoConfigurationCustomizerProvider {
     @Override
-    public void configure(SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
+    public void customize(AutoConfigurationCustomizer autoConfiguration) {
+        autoConfiguration.addTracerProviderCustomizer(this::configureSdkTracerProvider);
+    }
+
+
+    private SdkTracerProviderBuilder configureSdkTracerProvider(SdkTracerProviderBuilder tracerProvider, ConfigProperties config) {
         int sampleRate;
         try {
             sampleRate = EnvironmentConfiguration.getSampleRate();
@@ -22,7 +28,7 @@ public class HoneycombSdkTracerProviderConfigurer implements SdkTracerProviderCo
             sampleRate = 1;
         }
 
-        tracerProvider
+        return tracerProvider
             .setSampler(new DeterministicTraceSampler(sampleRate))
             .addSpanProcessor(new BaggageSpanProcessor());
     }
