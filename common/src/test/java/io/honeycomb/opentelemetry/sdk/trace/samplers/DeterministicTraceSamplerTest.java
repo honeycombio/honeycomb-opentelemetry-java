29654ac2af6c540b33ef9e595899077966592075
Graham Lea <graham@archium.io>
2022-06-28 22:31:32 +1000

    Remove explicit use of Sampler.alwaysOn() as decorated Sampler in tests

--- common/src/test/java/io/honeycomb/opentelemetry/sdk/trace/samplers/DeterministicTraceSamplerTest.java
+++ common/src/test/java/io/honeycomb/opentelemetry/sdk/trace/samplers/DeterministicTraceSamplerTest.java
@@ -20,7 +20,7 @@ public class DeterministicTraceSamplerTest {
     @Test
     public void test_neverSample() {
         int sampleRate = 0;
-        Sampler sampler = new DeterministicTraceSampler(Sampler.alwaysOn(), sampleRate);
+        Sampler sampler = new DeterministicTraceSampler(sampleRate);
 
         for (int i = 0; i < 100; i++) {
             String traceID = IdGenerator.random().generateTraceId();
@@ -32,7 +32,7 @@ public class DeterministicTraceSamplerTest {
     @Test
     public void test_alwaysSample() {
         int sampleRate = 1;
-        Sampler sampler = new DeterministicTraceSampler(Sampler.alwaysOn(), sampleRate);
+        Sampler sampler = new DeterministicTraceSampler(sampleRate);
 
         for (int i = 0; i < 100; i++) {
             String traceID = IdGenerator.random().generateTraceId();
@@ -45,7 +45,7 @@ public class DeterministicTraceSamplerTest {
     public void test_samplingResult_has_sampleRate_attribute() {
 
         for (int i = 0; i < 10; i++) {
-            Sampler sampler = new DeterministicTraceSampler(Sampler.alwaysOn(), i);
+            Sampler sampler = new DeterministicTraceSampler(i);
             String traceID = IdGenerator.random().generateTraceId();
             SamplingResult result = sampler.shouldSample(Context.current(), traceID, "span", SpanKind.CLIENT, Attributes.empty(), Collections.emptyList());
 
@@ -60,7 +60,7 @@ public class DeterministicTraceSamplerTest {
     public void test_deterministicSampler_varies_sampling_decision_based_on_trace_id() {
         int count = 0;
         for (int i = 0; i < 100; i++) {
-            Sampler sampler = new DeterministicTraceSampler(Sampler.alwaysOn(), 2);
+            Sampler sampler = new DeterministicTraceSampler(2);
             String traceID = IdGenerator.random().generateTraceId();
             SamplingResult result = sampler.shouldSample(Context.current(), traceID, "span", SpanKind.CLIENT, Attributes.empty(), Collections.emptyList());
             if (result.getDecision() == SamplingDecision.RECORD_AND_SAMPLE) {
