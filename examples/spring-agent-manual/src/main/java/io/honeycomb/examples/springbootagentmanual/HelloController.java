3eb4fb5e278b7dce77826a300d20aaf87dcf2716
Mike Goldsmth <goldsmith.mike@gmail.com>
2022-10-20 14:30:15 +0100

    update otel sdk and agent to v1.19.0/v1.19.1

--- examples/spring-agent-manual/src/main/java/io/honeycomb/examples/springbootagentmanual/HelloController.java
+++ examples/spring-agent-manual/src/main/java/io/honeycomb/examples/springbootagentmanual/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.opentelemetry.api.baggage.Baggage;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.extension.annotations.WithSpan;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @RestController
 public class HelloController {
