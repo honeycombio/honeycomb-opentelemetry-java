fae1c4819cf4b06c0a0ba850d530a4a40526b0d3
Mike Goldsmth <goldsmith.mike@gmail.com>
2024-04-10 15:42:54 +0100

    use deterministic sampler in sdk example

--- examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
+++ examples/spring-sdk/src/main/java/io/honeycomb/examples/springbootsdk/Application.java
@@ -1,6 +1,7 @@
 package io.honeycomb.examples.springbootsdk;
 
 import io.honeycomb.opentelemetry.OpenTelemetryConfiguration;
+import io.honeycomb.opentelemetry.sdk.trace.samplers.DeterministicTraceSampler;
 import io.honeycomb.opentelemetry.sdk.trace.spanprocessors.BaggageSpanProcessor;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 
@@ -13,7 +14,10 @@ public class Application {
     @Bean
     public OpenTelemetrySdk honeycomb() {
         return OpenTelemetryConfiguration.builder()
+            // add baggage span processor that copies baggage attributes to new spans
             .addSpanProcessor(new BaggageSpanProcessor())
+            // set deterministic trace sampler with sample rate of 1/1 (100%)
+            .setSampler(new DeterministicTraceSampler(1))
             .setApiKey(System.getenv("HONEYCOMB_API_KEY"))
             .setDataset(System.getenv("HONEYCOMB_DATASET"))
             .setServiceName(System.getenv("SERVICE_NAME"))
